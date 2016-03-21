772c95be8da2d8064fe53355f666ddbe1e367800
marczan2501 <marekjedrycha@hotmail.com>
2016-03-21 11:05:42 +0100

    upload

---
+++ DirectX Teapot/DXTest/DXTest/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace DXTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(370, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Os X";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(370, 36);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Os Y";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(370, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(49, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Os Z";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 351);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(370, 82);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Perspektywa";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(370, 105);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ortogonalne";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(370, 128);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(86, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 376);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Wyswietlanie Mesha";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ DirectX Teapot/DXTest/DXTest/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace DXTest
+{
+    public partial class Form1 : Form
+    {
+        private Mesh mesh;
+        private Device device;
+        private CustomVertex.PositionColored[] coloredsq;
+        float angle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            InitializeDirectX();
+        }
+
+        private void InitializeDirectX()
+        {
+            PresentParameters PP = new PresentParameters();
+            PP.Windowed = true;
+            PP.SwapEffect = SwapEffect.Discard;
+
+            PP.DeviceWindowHandle = pictureBox1.Handle;
+
+            device = new Device(0, DeviceType.Hardware, pictureBox1.Handle, CreateFlags.HardwareVertexProcessing, PP);
+            InitializeScene();
+        }
+
+        private void InitializeScene()
+        {
+            mesh = Mesh.Teapot(device);
+        }
+        private void Render()
+        {
+            float zoom = trackBar1.Value;
+
+            Vector3 rotate = new Vector3(0, 0, 0);
+
+            device.Clear(ClearFlags.Target, Color.Black, 1.0f, 0);
+
+            if (radioButton1.Checked)
+                device.Transform.Projection = Matrix.PerspectiveFovLH(45.0f, 1.0f, 0.1f, 100f);
+
+            if (radioButton2.Checked)
+                device.Transform.Projection = Matrix.OrthoOffCenterLH(-1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 100.0f);
+
+            if (checkBox1.Checked)
+                rotate.X = 1f;
+            else
+                rotate.X = 0f;
+
+            if (checkBox2.Checked)
+                rotate.Y = 1f;
+            else
+                rotate.Y = 0f;
+
+            if (checkBox3.Checked)
+                rotate.Z = 1f;
+            else
+                rotate.Z = 0f;
+                       
+            device.Transform.World = Matrix.RotationAxis(rotate, angle);
+            device.Transform.World *= Matrix.Translation(0f, 0f, zoom);
+
+            device.RenderState.Lighting = false;
+            device.RenderState.FillMode = FillMode.WireFrame;
+            device.RenderState.CullMode = Cull.None;
+
+            device.BeginScene();
+            mesh.DrawSubset(0);
+            
+            device.EndScene();
+            device.Present();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            device.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            angle += 0.1f;
+            Render();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+    }
+}
