7bd5162da50797c10e2dba5b0d4d873556d77971
marczan2501 <marekjedrycha@hotmail.com>
2016-03-21 10:56:35 +0100

    upload

---
+++ DirectX Trzy Cieniowania/DXTest/DXTest/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DXTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 351);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(370, 13);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FLAT DX";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(370, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(111, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "FLAT Modification";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(370, 59);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "GOURAUD";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(370, 82);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 6;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(129, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 376);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Trzy Cieniowania";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ DirectX Trzy Cieniowania/DXTest/DXTest/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace DXTest
+{
+    public partial class Form1 : Form
+    {
+        private Device device;
+        private Mesh mesh;
+        private Material material;
+        private CustomVertex.PositionNormalColored[] flatObject;
+
+        float angle = 0;
+        public List<Vector3> normalsToFace;
+        public List<Vector3> vertexes3D;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            InitializeDirectX();
+        }
+        private void InitializeDirectX()
+        {
+            PresentParameters PP = new PresentParameters();
+            PP.Windowed = true;
+            PP.SwapEffect = SwapEffect.Discard;
+
+            PP.AutoDepthStencilFormat = DepthFormat.D24S8;
+            PP.EnableAutoDepthStencil = true;
+
+            PP.DeviceWindowHandle = pictureBox1.Handle;
+            device = new Device(0, DeviceType.Hardware, pictureBox1.Handle, CreateFlags.HardwareVertexProcessing, PP);
+            InitializeScene();
+        }
+        private void InitializeScene()
+        {
+            mesh = Mesh.Teapot(device);
+
+            computeNormalsToFace(mesh);
+            prepareFlatObject(mesh);
+
+            material = new Material();
+            material.Diffuse = Color.Yellow;
+        }
+        public void computeNormalsToFace(Mesh mesh)
+        {
+            float[] vertices = (float[])mesh.LockVertexBuffer(typeof(float), LockFlags.ReadOnly, 6 * mesh.NumberVertices);
+            short[] faces = (short[])mesh.LockIndexBuffer(typeof(short), LockFlags.ReadOnly, 3 * mesh.NumberFaces);
+
+            normalsToFace = new List<Vector3>();
+
+            for (int i = 0; i < mesh.NumberFaces * 3; i += 3)
+            {
+                float x1 = vertices[faces[i] * 6 + 0];
+                float y1 = vertices[faces[i] * 6 + 1];
+                float z1 = vertices[faces[i] * 6 + 2];
+
+                float x2 = vertices[faces[i + 1] * 6 + 0];
+                float y2 = vertices[faces[i + 1] * 6 + 1];
+                float z2 = vertices[faces[i + 1] * 6 + 2];
+
+                float x3 = vertices[faces[i + 2] * 6 + 0];
+                float y3 = vertices[faces[i + 2] * 6 + 1];
+                float z3 = vertices[faces[i + 2] * 6 + 2];
+
+                Vector3 v1 = new Vector3(x1, y1, z1);
+                Vector3 v2 = new Vector3(x2, y2, z2);
+                Vector3 v3 = new Vector3(x3, y3, z3);
+
+                v1.Subtract(v3);
+                v2.Subtract(v3);
+
+                normalsToFace.Add(Vector3.Cross(v1, v2));
+            }
+            mesh.UnlockIndexBuffer();
+            mesh.UnlockVertexBuffer();
+        }
+        private void prepareFlatObject(Mesh mesh)
+        {
+            float[] verticles = (float[])mesh.LockVertexBuffer(typeof(float), LockFlags.ReadOnly, 6 * mesh.NumberVertices);
+            short[] faces = (short[])mesh.LockIndexBuffer(typeof(short), LockFlags.ReadOnly, 3 * mesh.NumberFaces);
+
+            flatObject = new CustomVertex.PositionNormalColored[mesh.NumberFaces * 3];
+
+            for (int i = 0; i < faces.Length; i++)
+            {
+                flatObject[i].X = verticles[faces[i] * 6 + 0];
+                flatObject[i].Y = verticles[faces[i] * 6 + 1];
+                flatObject[i].Z = verticles[faces[i] * 6 + 2];
+                flatObject[i].Normal = normalsToFace[i / 3];
+                flatObject[i].Color = Color.Yellow.ToArgb();
+            }
+
+            mesh.UnlockIndexBuffer();
+            mesh.UnlockVertexBuffer();
+        }
+        private void Render()
+        {
+            device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+
+            float zoom = trackBar1.Value;
+            float aspect = (float)pictureBox1.Width / pictureBox1.Height;
+
+            device.Transform.Projection = Matrix.PerspectiveFovLH(45f, aspect, 0.1f, 1000f);
+            device.Transform.World = Matrix.RotationAxis(new Vector3(0, 1, 0), angle);
+            device.Transform.World *= Matrix.Translation(0f, 0f, zoom/2);
+
+            device.Lights[0].Type = LightType.Directional;
+            device.Lights[0].Direction = new Vector3(0.0f, 0.0f, 1.0f);
+            device.Lights[0].Enabled = true;
+            device.RenderState.CullMode = Cull.None;
+
+            device.RenderState.NormalizeNormals = true;
+
+            device.BeginScene();
+            device.Material = material;
+
+            if (radioButton1.Checked)
+            {
+                device.RenderState.ShadeMode = ShadeMode.Flat;
+                mesh.DrawSubset(0);
+            }
+
+            if (radioButton2.Checked)
+            {
+                device.VertexFormat = CustomVertex.PositionNormalColored.Format;
+                device.DrawUserPrimitives(PrimitiveType.TriangleList, flatObject.Length / 3, flatObject);
+            }
+
+            if (radioButton3.Checked)
+            {
+                device.RenderState.ShadeMode = ShadeMode.Gouraud;
+                mesh.DrawSubset(0);
+            }
+            device.EndScene();
+            device.Present();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            device.Dispose();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            angle += 0.1f;
+            Render();
+            Invalidate();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+    }
+}
