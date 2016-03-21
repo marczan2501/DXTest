4967200b05bc4f92cf8640e0f654d0c758252a1e
marczan2501 <marekjedrycha@hotmail.com>
2016-03-21 11:10:10 +0100

    upload

---
+++ DirectX Sfery/DXTest/DXTest/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace DXTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 351);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(373, 13);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(86, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 376);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Wyswietlanie Sfer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DirectX Sfery/DXTest/DXTest/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace DXTest
+{
+    public partial class Form1 : Form
+    {
+        private Mesh mesh1;
+        private Mesh mesh2, mesh3;
+        private Device device;
+        private Material material;
+        float angle1 = 0;
+        float angle2 = 0;
+        float angle3 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            InitializeDirectX();
+        }
+
+        private void InitializeDirectX()
+        {
+            PresentParameters PP = new PresentParameters();
+
+            PP.AutoDepthStencilFormat = DepthFormat.D24X8;
+            PP.EnableAutoDepthStencil = true;
+
+            PP.Windowed = true;
+            PP.SwapEffect = SwapEffect.Discard;
+
+            PP.DeviceWindowHandle = pictureBox1.Handle;
+
+            device = new Device(0, DeviceType.Hardware, pictureBox1.Handle, CreateFlags.HardwareVertexProcessing, PP);
+            InitializeScene();
+        }
+
+        private void InitializeScene()
+        {
+
+            mesh1 = Mesh.Sphere(device, 10, 20, 20);
+            mesh1.ComputeNormals();
+            mesh2 = Mesh.Sphere(device, 10, 20, 20);
+            mesh2.ComputeNormals();
+            mesh3 = Mesh.Sphere(device, 10, 20, 20);
+            mesh3.ComputeNormals();
+
+            material = new Material();
+            material.Diffuse = Color.Yellow;
+        }
+
+        private void SetRenderState()
+        {
+            device.RenderState.NormalizeNormals = true;
+            device.RenderState.CullMode = Cull.None;
+            device.RenderState.ShadeMode = ShadeMode.Gouraud;
+            device.RenderState.Lighting = true;
+        }
+
+        private void SetLight()
+        {
+            device.Lights[0].Type = LightType.Directional;
+            device.Lights[0].Position = new Vector3(0.0f, 0.0f, 40.0f);
+            device.Lights[0].Direction = new Vector3(0.0f, 0.0f, 1.0f);
+            device.Lights[0].Enabled = true;
+
+        }
+        private void Render()
+        {
+            SetRenderState();
+            SetLight();
+            float zoom = trackBar1.Value;
+
+            label1.Text = zoom.ToString();
+
+            Vector3 rotate = new Vector3(0, 0, 0);
+
+            device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+
+            device.Transform.Projection = Matrix.PerspectiveFovLH(45.0f, 1.0f, 0.1f, 1000f);
+            
+            device.Material = material;
+            device.BeginScene();
+
+            device.Transform.World = Matrix.RotationAxis(new Vector3(1,1,1), angle1);
+            device.Transform.World *= Matrix.Translation(0f, 0f, zoom);
+            mesh1.DrawSubset(0);
+
+            device.Transform.World = Matrix.Translation(0f, 0f, zoom);
+            device.Transform.World *= Matrix.Translation(0f, 0f, -zoom-100f);
+            device.Transform.World *= Matrix.RotationAxis(new Vector3(0, 1, 0), angle2);
+            device.Transform.World *= Matrix.Translation(0f, 0f, zoom+100f);
+            mesh2.DrawSubset(0);
+
+            device.Transform.World = Matrix.Translation(0f, 0f, zoom);
+            device.Transform.World *= Matrix.Translation(0f, 0f, -zoom-100);
+            device.Transform.World *= Matrix.RotationAxis(new Vector3(1, 0, 0), angle3);
+            device.Transform.World *= Matrix.Translation(0f, 0f, zoom+100f);
+            mesh3.DrawSubset(0);
+
+            device.EndScene();
+            device.Present();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            device.Dispose();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            angle1 += 0.01f;
+            angle2 += 0.02f;
+            angle3 += 0.03f;
+            Render();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Render();
+        }
+    }
+}
